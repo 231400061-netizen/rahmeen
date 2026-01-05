03373916d416340d7e9c84c1137823b4c023bc8e
231400061-netizen <231400061@gift.edu.pk>
2026-01-05 15:23:03 +0500

    Update Animal.java

--- Animal.java
+++ Animal.java
@@ -1,1 +1,17 @@
+public class Animal {
+    String name;
+
+    Animal(String name) {
+        this.name = name;
+    }
+
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+
+    public static void main(String[] args) {
+        Animal a = new Animal("Dog");
+        a.sound();
+    }
+}
 
